523158b2ffa9e205643aa82e34e31675531ba8bb
Letícia <161726939+leletis09@users.noreply.github.com>
2024-10-01 12:52:13 -0300

    Add files via upload

--- arteconnect/cadastro/cadastro.js
+++ arteconnect/cadastro/cadastro.js
@@ -14,16 +14,16 @@ function LoginEmpresa() {
   }
 
 
-function Login() {
-    const usuario = document.getElementById('usuario').value;
-    const senha = document.getElementById('senha').value;
+// function Login() {
+//     const usuario = document.getElementById('usuario').value;
+//     const senha = document.getElementById('senha').value;
 
-    if (usuario === "josue" || senha === "111") {
-        window.location.href = "cadastro.html";
-        return true;
-    }
-    else {
-    alert("Por favor, preencha todos os campos.");
-    return false; // Retorna false para evitar o redirecionamento}
-}
-}
+//     if (usuario === "josue" || senha === "111") {
+//         window.location.href = "cadastro.html";
+//         return true;
+//     }
+//     else {
+//     alert("Por favor, preencha todos os campos.");
+//     return false; // Retorna false para evitar o redirecionamento}
+// }
+// }
--- arteconnect/script.js
+++ arteconnect/script.js
@@ -54,7 +54,27 @@ document.addEventListener("DOMContentLoaded", () => {
           registerScreen.style.display = 'block';
       });
   });
-  function togglemenu() {
-    let submenu = document.getElementById("sub-menu-wrap"); // Correção: getElementById em vez de getElementsById
-    submenu.classList.toggle("open-menu");
-}
+//   function togglemenu() {
+//     let submenu = document.getElementById("sub-menu-wrap"); // Correção: getElementById em vez de getElementsById
+//     submenu.classList.toggle("open-menu");
+// }
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
